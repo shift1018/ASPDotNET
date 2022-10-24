5099e517b9bc898557bf2c5ebdc69bc90db87bb6
shift1018 <105242991+shift1018@users.noreply.github.com>
2022-10-24 12:50:43 -0400

    shift

---
+++ Bakery/Data/BakeryContext.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bakery.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Bakery.Data
+{
+    public class BakeryContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data source=Bakery.db");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new ProductConfiguration());
+    }
+    }
+}
